41ffabe82bb2ce314e8ba2b0751e64a78b74ef94
Akihiro Yamamoto <fkmks247@gmail.com>
2014-07-27 00:17:21 +0900

    Tried component

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+var blink = document.querySelector('.blink');
+
+setInterval(function () {
+  blink.style.visibility = getComputedStyle(blink).visibility === 'hidden'
+    ? 'visible'
+    : 'hidden';
+}, 300);
